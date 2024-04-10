077b2a60e7c93fcd052086b1b26669b451507f2b
IsaacManoel <162491159+IsaacManoel@users.noreply.github.com>
2024-04-10 15:44:17 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dado_Rolador
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dado_Rolador
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
